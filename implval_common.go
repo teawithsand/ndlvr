8a5215cdd248efa873e53f4a938e3ce9dcf12974
teawithsand <teawithsand@gmail.com>
2022-03-06 20:59:19 +0000

    Refactored basic validation for new single design

--- implval_common.go
+++ implval_common.go
@@ -1,39 +1,27 @@
 package livr
 
 import (
-	"errors"
-
 	"github.com/teawithsand/livr4go/value"
 )
 
-var ErrEmpty = errors.New("livr: field must not be empty")
-
 func makeRequiredVF() (vf ValidationFactory) {
-	vf = ValidationAsFactory(func(bctx ValidationBuildContext, vv value.Value) (err error) {
-		_, err = value.ExpectKeyedValueField(vv, bctx.Data.FieldName, true)
-		if err != nil {
-			return
-		}
+	vf = SimpleFieldValidation(true, func(bctx ValidationBuildContext, vv, fieldValue value.Value) (err error) {
 		return
 	})
-
 	vf = WrapNamed("required", vf)
 	return
 }
 
 func makeNotEmptyVF() (vf ValidationFactory) {
-	vf = ValidationAsFactory(func(bctx ValidationBuildContext, vv value.Value) (err error) {
-		fieldValue, err := value.ExpectKeyedValueField(vv, bctx.Data.FieldName, true)
-		if err != nil {
+	vf = SimpleFieldValidation(
+		true,
+		func(bctx ValidationBuildContext, vv, fieldValue value.Value) (err error) {
+			if bctx.OPs.IsEmpty(fieldValue) {
+				err = MakeLIVRError("Field must not be empty", "NOT_EMPTY")
+				return
+			}
 			return
-		}
-
-		if bctx.OPs.IsEmpty(fieldValue) {
-			err = ErrEmpty
-			return
-		}
-		return
-	})
+		})
 
 	vf = WrapNamed("required", vf)
 	return
